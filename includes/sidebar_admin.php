256555bb733e17e647a8965adacb2f57be6b7ac7
Estephanie Anne De Torres <140788832+estephaniedetorres@users.noreply.github.com>
2026-04-08 13:48:22 +0800

    Replace SVG logo with image logo in sidebar

--- includes/sidebar_admin.php
+++ includes/sidebar_admin.php
@@ -7,10 +7,8 @@ $currentPage = basename($_SERVER['PHP_SELF'], '.php');
     <div class="p-5 border-b border-neutral-800">
         <div class="flex items-center justify-between">
             <div class="flex items-center gap-3">
-            <div class="w-10 h-10 bg-white rounded-lg flex items-center justify-center">
-                <svg class="w-6 h-6 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
-                </svg>
+            <div class="w-20 h-20 bg-white rounded-lg flex items-center justify-center">
+                <img class="w-15 h-15 text-black" src="/img/uca-logo.png" alt="">
             </div>
             <div>
                 <h1 class="text-lg font-bold">Grading Management System</h1>
